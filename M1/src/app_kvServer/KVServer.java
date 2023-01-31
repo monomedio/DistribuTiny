e40cc60e98b1f2fb2347651c1f9a3aff6d3d9b37
juanjayala <juan.j.ayala@outlook.com>
2023-01-31 17:01:19 -0500

    Server command line arguments implemented

--- M1/src/app_kvServer/KVServer.java
+++ M1/src/app_kvServer/KVServer.java
@@ -9,6 +9,11 @@ import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.InetAddress;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 public class KVServer implements IKVServer {
     private static Logger logger = Logger.getRootLogger();
@@ -25,6 +30,8 @@ public class KVServer implements IKVServer {
 
     private Storage store;
 
+    private InetAddress address;
+
     /**
      * Start KV Server at given port
      *
@@ -35,12 +42,17 @@ public class KVServer implements IKVServer {
      *                  is full and there is a GET- or PUT-request on a key that is
      *                  currently not contained in the cache. Options are "FIFO", "LRU",
      *                  and "LFU".
+     * @param address  specifies which address the server should listen to.
+     *                 set to localhost by default
+     *
      */
-    public KVServer(int port, int cacheSize, String strategy, String path) {
+    public KVServer(int port, int cacheSize, String strategy, String path, InetAddress address) {
         this.port = port;
         this.cacheSize = cacheSize;
         this.cacheStrategy = CacheStrategy.valueOf(strategy);
         this.store = new Storage(path);
+
+        this.address = address;
     }
 
     @Override
@@ -139,7 +151,7 @@ public class KVServer implements IKVServer {
     private boolean initializeServer() {
         logger.info("Initialize server...");
         try {
-            this.serverSocket = new ServerSocket(port);
+            this.serverSocket = new ServerSocket(port, 50, address);
             logger.info("Server listening on port: "
                     + serverSocket.getLocalPort());
             return true;
@@ -171,24 +183,135 @@ public class KVServer implements IKVServer {
         }
     }
 
+    // HELPERS
+    private static Level getLevel(String levelString) {
+
+        if(levelString.equals(Level.ALL.toString())) {
+            return Level.ALL;
+        } else if(levelString.equals(Level.DEBUG.toString())) {
+            return Level.DEBUG;
+        } else if(levelString.equals(Level.INFO.toString())) {
+            return Level.INFO;
+        } else if(levelString.equals(Level.WARN.toString())) {
+            return Level.WARN;
+        } else if(levelString.equals(Level.ERROR.toString())) {
+            return Level.ERROR;
+        } else if(levelString.equals(Level.FATAL.toString())) {
+            return Level.FATAL;
+        } else if(levelString.equals(Level.OFF.toString())) {
+            return Level.OFF;
+        } else {
+//            return LogSetup.UNKNOWN_LEVEL;
+            return null;
+        }
+    }
+
+    private static void printHelp() {
+        String s = "KVSERVER HELP (Usage):\n" +
+                "::::::::::::::::::::::::::::::::" +
+                "::::::::::::::::::::::::::::::::\n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n" +
+                "%-30.30s  %-30s%n";
+        System.out.printf(s,
+                "-p", "Sets the port of the server",
+                "-a", "Which address the server should listen to, set the default to localhost. Default: 127.0.0.1",
+                "-d", "Directory for files (Put here the files you need to persist the data)",
+                "-l", "Relative path of the logfile. Default: file 'echo.log' in current directory",
+                "-ll", "Loglevel. Default: ALL.",
+                "", "| Possible log levels are:",
+                "", "| ALL | DEBUG | INFO | WARN | ERROR | FATAL | OFF",
+                "-csz", "Cache size. Specifies how many key-value pairs the server is allowed to keep in-memory",
+                "-cst", "Specifies the cache replacement strategy in case the cache is full. " +
+                        "Options are FIFO, LRU, and LFU.",
+                "-h", "Display the help.");
+    }
+
     public static void main(String[] args) {
         try {
-            new LogSetup("logs/server.log", Level.ALL);
-            if (args.length != 4) {
-                System.out.println("Error! Invalid number of arguments");
-                System.out.println("Usage: KVServer <port> <cache size> <cache strategy> <storage_path>");
-            } else {
-                int port = Integer.parseInt(args[0]);
-                int cacheSize = Integer.parseInt(args[1]);
-                new KVServer(port, cacheSize, args[2], args[3]).run();
+            // set variables to default
+            boolean help = false;
+
+            int port = -1;
+            InetAddress addr = InetAddress.getByName("127.0.0.1");
+            String stPath = null;
+            String logDir = "server.log"; // default is curr directory
+            String logLevelStr = "ALL";
+            int cSize = -1;
+            String cStrat = null;
+
+            // convert args to an ArrayList
+            List<String> tokens = new ArrayList<String>(Arrays.asList(args));
+
+            // parse commandline arguments
+            while (!tokens.isEmpty()){
+                String curr = tokens.get(0);
+
+                if (curr.equals("-h")) {
+                    help = true;
+                    printHelp();
+
+                    break;
+                } else {
+                    switch (curr) {
+                        case "-p":
+                            // sets the port of the server
+                            port = Integer.parseInt(tokens.get(1));
+                            break;
+                        case "-a":
+                            // which address the server should listen to
+                            addr = InetAddress.getByName(tokens.get(1));
+                            break;
+                        case "-d":
+                            // storage path / directory for files
+                            stPath = tokens.get(1);
+                            break;
+                        case "-l":
+                            // relative path of the logfile
+                            logDir = tokens.get(1);
+                            break;
+                        case "-ll":
+                            // LogLevel, e.g., INFO, ALL, ...,
+                            logLevelStr = tokens.get(1);
+                            break;
+                        case "-csz":
+                            // cache size
+                            cSize = Integer.parseInt(tokens.get(1));
+                            break;
+                        case "-cst":
+                            cStrat = tokens.get(1);
+                            break;
+                    }
+                    tokens.remove(1);
+                    tokens.remove(0);
+                }
+            }
+
+            // all are parsed
+            if (!help) {
+                Level logLevel = getLevel(logLevelStr);
+
+                new LogSetup(logDir, logLevel);
+                new KVServer(port, cSize, cStrat, stPath, addr).run();
             }
         } catch (IOException e) {
             System.out.println("Error! Unable to initialize logger!");
             e.printStackTrace();
             System.exit(1);
         } catch (NumberFormatException nfe) {
-            System.out.println("Error! Invalid argument <port>! Not a number!");
-            System.out.println("Usage: KVServer <port> <cache size> <cache strategy>");
+//            System.out.println("Error! Invalid argument <port>! Not a number!");
+//            System.out.println("Usage: KVServer <port> <cache size> <cache strategy>");
+            System.exit(1);
+        } catch (NullPointerException e) {
+            System.out.println("Error! No argument given for mandatory variables.");
+            printHelp();
             System.exit(1);
         }
     }
