0e7c79c4aa5ef105ac4b755d772294ce470fbb62
juanjayala <juan.j.ayala@outlook.com>
2023-02-03 21:43:07 -0500

    added tests for interactions with two clients

--- M1/src/testing/InteractionTest.java
+++ M1/src/testing/InteractionTest.java
@@ -1,5 +1,6 @@
 package testing;
 
+import app_kvServer.KVServer;
 import org.junit.Test;
 
 import client.KVStore;
@@ -11,11 +12,14 @@ import shared.messages.IKVMessage.StatusType;
 public class InteractionTest extends TestCase {
 
 	private KVStore kvClient;
+	private KVStore kvClient2;
 	
 	public void setUp() {
 		kvClient = new KVStore("localhost", 50000);
+		kvClient2 = new KVStore("localhost", 50000);
 		try {
 			kvClient.connect();
+			kvClient2.connect();
 
 		} catch (Exception e) {
 		}
@@ -131,7 +135,102 @@ public class InteractionTest extends TestCase {
 
 		assertTrue(ex == null && response.getStatus() == StatusType.GET_ERROR);
 	}
-	
+
+
+	// Our Test Cases
+	@Test
+	public void testMessageSizeExceeded() {
+		String key = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+				"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+		String value = "hi";
+		IKVMessage response = null;
+		Exception ex = null;
+
+		try {
+			response = kvClient.put(key, value);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.FAILED);
+
+	}
+
+	@Test
+	public void testGetDeletedValue() {
+		String key = "k1";
+		String value = "v1";
+		IKVMessage response = null;
+		Exception ex = null;
+
+		try {
+			kvClient.put(key, value);
+			kvClient.put(key, "null");
+			response = kvClient.get(key);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null && response.getStatus() == StatusType.GET_ERROR);
+	}
+
+	@Test
+	public void testTwoClientsGet() {
+		String key = "k2";
+		String value = "v2";
+		IKVMessage response = null;
+		Exception ex = null;
+
+		try {
+			kvClient.put(key, value);
+			response = kvClient2.get(key);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null
+				&& response.getStatus() == StatusType.GET_SUCCESS && response.getValue().equals(value));
+	}
+
+	@Test
+	public void testTwoClientsUpdate() {
+		String key = "k3";
+		String value = "v3";
+		IKVMessage response = null;
+		Exception ex = null;
+
+		try {
+			kvClient.put(key, value);
+			kvClient2.put(key, "v3-update");
+			response = kvClient.get(key);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null
+				&& response.getStatus() == StatusType.GET_SUCCESS && response.getValue().equals("v3-update"));
+	}
+
+	@Test
+	public void testTwoClientsDeleteGet() {
+		String key = "k4";
+		String value = "v4";
+		IKVMessage response = null;
+		Exception ex = null;
+
+		try {
+			kvClient.put(key, value);
+			kvClient2.put(key, "null");
+			response = kvClient.get(key);
+		} catch (Exception e) {
+			ex = e;
+		}
+
+		assertTrue(ex == null
+				&& response.getStatus() == StatusType.GET_ERROR);
+	}
 
 
 }
