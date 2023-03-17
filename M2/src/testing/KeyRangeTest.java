23ea3b05d42253857d7dc94f338efdd318ac1250
juanjayala <juan.j.ayala@outlook.com>
2023-03-17 18:11:10 -0400

    KVServer KeyInRange modified + KeyRangeTests modified

--- M2/src/testing/KeyRangeTest.java
+++ M2/src/testing/KeyRangeTest.java
@@ -6,103 +6,103 @@ import org.junit.Test;
 public class KeyRangeTest {
 
     public boolean keyInRange(String hashedKey, String lowerRange, String upperRange){
-        if (hashedKey.compareTo(lowerRange) == 0) {
+        if (hashedKey.compareTo(upperRange) == 0) {
             return true;
         }
-        // if lowerRange is larger than upperRange
-        if (lowerRange.compareTo(upperRange) > 0) {
-            // hashedkey <= lowerRange and hasedkey > upperRange
-            return ((hashedKey.compareTo(lowerRange) <= 0) && hashedKey.compareTo(upperRange) > 0);
+        // if upperRange is larger than lowerRange
+        if (upperRange.compareTo(lowerRange) > 0) {
+            // hashedkey <= upperRange and hashedkey > lowerRange
+            return ((hashedKey.compareTo(upperRange) <= 0) && hashedKey.compareTo(lowerRange) > 0);
         } else {
-            // lowerRange is smaller than upperRange (wrap around)
-            return ((hashedKey.compareTo(lowerRange) <= 0 && (upperRange.compareTo(hashedKey) > 0))) ||
-                    ((hashedKey.compareTo(lowerRange) > 0) && (upperRange.compareTo(hashedKey) < 0));
+            // upperRange is smaller than lowerRange (wrap around)
+            return ((hashedKey.compareTo(upperRange) <= 0 && (lowerRange.compareTo(hashedKey) > 0))) ||
+                    ((hashedKey.compareTo(upperRange) > 0) && (lowerRange.compareTo(hashedKey) < 0));
         }
     }
 
     @Test
-    public void no_wrap_around_hashed_eq_lower(){
-        // Case 1: lowerRange is larger than upperRange
-        String lowerRange = "c".repeat(32);
-        String upperRange = "a".repeat(32);
+    public void no_wrap_around_hashed_eq_upper(){
+        // Case 1: upperRange is larger than lowerRange
+        String lowerRange = "1".repeat(32);
+        String upperRange = "5".repeat(32);
 
-        // Subcase 1.1: hashedKey == lowerRange, is in range
-        String hashedKey1 = "c".repeat(32);
+        // Subcase 1.1: hashedKey == upperRange, is in range
+        String hashedKey1 = "5".repeat(32);
         assert(keyInRange(hashedKey1, lowerRange, upperRange));
     }
 
     @Test
     public void no_wrap_around_hashed_btwn_lower_upper(){
-        // Case 1: lowerRange is larger than upperRange
-        String lowerRange = "c".repeat(32);
-        String upperRange = "a".repeat(32);
+        // Case 1: upperRange is larger than lowerRange
+        String lowerRange = "1".repeat(32);
+        String upperRange = "5".repeat(32);
 
-        // Subcase 1.2: upperRange < hashedKey < lowerRange, is in range
-        String hashedKey2 = "b".repeat(32);
+        // Subcase 1.2: lowerRange < hashedKey < upperRange, is in range
+        String hashedKey2 = "3".repeat(32);
         assert(keyInRange(hashedKey2, lowerRange, upperRange));
     }
 
     @Test
-    public void no_wrap_around_hashed_eq_upper(){
-        // Case 1: lowerRange is larger than upperRange
-        String lowerRange = "c".repeat(32);
-        String upperRange = "a".repeat(32);
+    public void no_wrap_around_hashed_eq_lower(){
+        // Case 1: upperRange is larger than lowerRange
+        String lowerRange = "1".repeat(32);
+        String upperRange = "5".repeat(32);
 
-        // Subcase 1.3: hashedKey == upperRange, NOT in range
-        String hashedKey3 = "a".repeat(32);
+        // Subcase 1.3: hashedKey == lowerRange, NOT in range
+        String hashedKey3 = "1".repeat(32);
         assert(!keyInRange(hashedKey3, lowerRange, upperRange));
     }
 
     @Test
     public void no_wrap_around_hashed_less_upper(){
-        // Case 1: lowerRange is larger than upperRange
-        String lowerRange = "c".repeat(32);
-        String upperRange = "a".repeat(32);
+        // Case 1: upperRange is larger than lowerRange
+        String lowerRange = "1".repeat(32);
+        String upperRange = "5".repeat(32);
 
-        // Subcase 1.4: hashedKey < upperRange, NOT in range
+        // Subcase 1.4: hashedKey < lowerRange, NOT in range
         String hashedKey4 = "0".repeat(32);
         assert(!keyInRange(hashedKey4, lowerRange, upperRange));
     }
 
     @Test
     public void no_wrap_around_hashed_greater_lower(){
-        // Case 1: lowerRange is larger than upperRange
-        String lowerRange = "c".repeat(32);
-        String upperRange = "a".repeat(32);
+        // Case 1: upperRange is larger than lowerRange
+        String lowerRange = "1".repeat(32);
+        String upperRange = "5".repeat(32);
 
-        // Subcase 1.5: hashedKey > lowerRange, NOT in range
-        String hashedKey5 = "f".repeat(32);
+        // Subcase 1.5: hashedKey > upperRange, NOT in range
+        String hashedKey5 = "d".repeat(32);
         assert(!keyInRange(hashedKey5, lowerRange, upperRange));
     }
 
 
     @Test
-    public void wrap_around_hashed_eq_lower(){
-        // Case 2: lowerRange is smaller than upperRange (wrap around)
-        String lowerRange = "b".repeat(32);
-        String upperRange = "e".repeat(32);
+    public void wrap_around_hashed_eq_upper(){
+        // Case 2: upperRange is smaller than lowerRange (wrap around)
+        String lowerRange = "e".repeat(32);
+        String upperRange = "3".repeat(32);
 
-        // Subcase 2.1: hashedKey == lowerRange, is in range
-        String hashedKey1 = "b".repeat(32);
+        // Subcase 2.1: hashedKey == upperRange, is in range
+        String hashedKey1 = "3".repeat(32);
         assert(keyInRange(hashedKey1, lowerRange, upperRange));
     }
 
     @Test
     public void wrap_around_hashed_less_lower_and_hashed_less_upper(){
-        // Case 2: lowerRange is smaller than upperRange (wrap around)
-        String lowerRange = "b".repeat(32);
-        String upperRange = "e".repeat(32);
+        // Case 2: upperRange is smaller than lowerRange (wrap around)
+        String lowerRange = "e".repeat(32);
+        String upperRange = "3".repeat(32);
 
         // Subcase 2.2: hashedKey < lowerRange AND hashedKey < upperRange, is in range
-        String hashedKey2 = "a".repeat(32);
+        String hashedKey2 = "1".repeat(32);
         assert(keyInRange(hashedKey2, lowerRange, upperRange));
     }
 
     @Test
     public void wrap_around_hashed_greater_lower_and_hashed_greater_upper(){
-        // Case 2: lowerRange is smaller than upperRange (wrap around)
-        String lowerRange = "b".repeat(32);
-        String upperRange = "e".repeat(32);
+        // Case 2: upperRange is smaller than lowerRange (wrap around)
+        String lowerRange = "e".repeat(32);
+        String upperRange = "3".repeat(32);
 
         // Subcase 2.3: hashedKey > lowerRange AND hashedKey > upperRange, is in range
         String hashedKey3 = "f".repeat(32);
@@ -110,24 +110,24 @@ public class KeyRangeTest {
     }
 
     @Test
-    public void wrap_around_hashed_eq_upper(){
-        // Case 2: lowerRange is smaller than upperRange (wrap around)
-        String lowerRange = "b".repeat(32);
-        String upperRange = "e".repeat(32);
+    public void wrap_around_hashed_eq_lower(){
+        // Case 2: upperRange is smaller than lowerRange (wrap around)
+        String lowerRange = "e".repeat(32);
+        String upperRange = "3".repeat(32);
 
-        // Subcase 2.4: hashedKey == upperRange, NOT in range
+        // Subcase 2.4: hashedKey == lowerRange, NOT in range
         String hashedKey4 = "e".repeat(32);
         assert(!keyInRange(hashedKey4, lowerRange, upperRange));
     }
 
     @Test
     public void wrap_around_hashed_greater_lower_and_hashed_less_upper(){
-        // Case 2: lowerRange is smaller than upperRange (wrap around)
-        String lowerRange = "b".repeat(32);
-        String upperRange = "e".repeat(32);
+        // Case 2: upperRange is smaller than lowerRange (wrap around)
+        String lowerRange = "e".repeat(32);
+        String upperRange = "3".repeat(32);
 
-        // Subcase 2.5: hashedKey > lowerRange AND hashedKey < upperRange, NOT in range
-        String hashedKey5 = "d".repeat(32);
+        // Subcase 2.5: hashedKey > upperRange AND hashedKey < lowerRange, NOT in range
+        String hashedKey5 = "a".repeat(32);
         assert(!keyInRange(hashedKey5, lowerRange, upperRange));
     }
 
@@ -135,17 +135,17 @@ public class KeyRangeTest {
     public void hash_at_ends_hashed_less_upper(){
         // hashedKey < upperRange (case with hashkey on other side of wrap around), NOT in range
         String lowerRange = "c".repeat(32);
-        String upperRange = "0".repeat(32);
-        String hashedKey1 = "f".repeat(32);
+        String upperRange = "f".repeat(32);
+        String hashedKey1 = "0".repeat(32);
         assert(!keyInRange(hashedKey1, lowerRange, upperRange));
     }
 
     @Test
     public void hash_at_ends_hashed_greater_lower(){
         // hashedKey > lowerRange (case with hashkey on other side of wrap around), NOT in range
-        String lowerRange = "f".repeat(32);
-        String upperRange = "d".repeat(32);
-        String hashedKey2 = "0".repeat(32);
+        String lowerRange = "0".repeat(32);
+        String upperRange = "5".repeat(32);
+        String hashedKey2 = "f".repeat(32);
         assert(!keyInRange(hashedKey2, lowerRange, upperRange));
     }
 
